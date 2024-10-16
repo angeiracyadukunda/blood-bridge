d2ee87d6f0d3f32b7fa11d9915d759f9f2dbdd66
GedeonMatabaro <mugishomatgedeon@gmail.com>
2024-10-17 00:57:43 +0200

    updating the env

--- Backend/firebase/firebaseAdmin.js
+++ Backend/firebase/firebaseAdmin.js
@@ -1,9 +1,10 @@
 
 require('dotenv').config(); 
 const admin = require('firebase-admin');
-const serviceAccount = require('./secret/blood-bridge-b30c8-firebase-adminsdk-jfofu-ea99deb941.json'); // Update the path to your .json file
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
 
 admin.initializeApp({
+  
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL, // Make sure this is set in your environment variables
 });
